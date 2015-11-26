72e91e30c52e7dd6ffb71870ce47a18cdfda70d3
Arpad Ryszka <arpad.ryszka@zalando.de>
2015-11-26 11:43:39 +0100

    add google-google script

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # google-google-noredirect
-small greasemonkey script to hide the blocking popup on google and to replace the home calling links with the real urls
+
+This is a small greasemonkey script to hide the blocking popup on google and to replace the home calling links
+with the real urls. It is based on and depends on the current DOM of google.de, so it may not be useful in
+general.
---
+++ google-google-noredirect.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @run-at      document-start
+// @name        google google
+// @namespace   google google google
+// @description google google duckduckgo
+// @include     https://www.google.*
+// @version     1
+// @grant       GM_addStyle
+// ==/UserScript==
+
+GM_addStyle("#lb { display: none; }");
+
+const clipNoScroll = () => {
+  ((html) => { html.className = html.className.replace(/\bnsc\b/, ""); })
+  (document.querySelector("html"));
+};
+
+const takeCite = (link) => {
+  var link = link.parentNode.parentNode.querySelector("cite").innerHTML
+    .replace(/(<[\/]?\w+>)|(\s+)/g, "")
+    .replace(/›.*/, "");
+
+  if (link.indexOf("https://") < 0 && link.indexOf("http://") < 0) {
+    link = "http://" + link;
+  }
+
+  return link;
+}
+
+const replaceLink = (link) => {
+  try {
+    const clone = document.createElement("a");
+    clone.innerHTML = "[FX] " + link.innerHTML;
+    clone.href = takeCite(link);
+    link.parentNode.replaceChild(clone, link);
+  } catch (err) {
+    console.log(err.toString());
+  }
+};
+
+const replaceResultLinks = () => {
+  [].forEach.call(document.querySelectorAll(".r > a"), replaceLink);
+};
+
+document.addEventListener("readystatechange", function () {
+  clipNoScroll();
+  if (document.readyState === "interactive") {
+    replaceResultLinks();
+  }
+});
